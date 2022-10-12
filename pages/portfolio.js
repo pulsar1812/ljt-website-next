a28cf762a36a92281ecb257d7bd2cb32c6088560
pulsar1812 <cluster000@gmail.com>
2022-10-12 23:53:06 +0800

    Remove more files

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import NavbarTwo from '../components/Layouts/NavbarTwo'
 import PageBanner from '../components/Common/PageBanner'
-import WorksStyleOne from '../components/Portfolio/WorksStyleOne'
 import WorksStyleTwo from '../components/Portfolio/WorksStyleTwo'
 import CtaAreaTwo from '../components/Common/CtaAreaTwo'
 
@@ -12,8 +11,6 @@ class Portfolio extends Component {
         <NavbarTwo />
 
         <PageBanner pageTitle='Our Portfolio' BGImage='bg-three' />
-
-        <WorksStyleOne />
 
         <WorksStyleTwo />
 
