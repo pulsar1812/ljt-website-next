466c64761ae9bb8dee9bd28e8b6149cf125a6156
pulsar1812 <cluster000@gmail.com>
2022-10-26 21:39:06 +0800

    Add contact email functionality

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,34 +1,28 @@
-import nodemailer from 'nodemailer'
-import sgTransport from 'nodemailer-sendgrid-transport'
-
-const transporter = {
-  auth: {
-    // Update your SendGrid API key here
-    api_key: '...',
-  },
-}
-
-const mailer = nodemailer.createTransport(sgTransport(transporter))
+import sendEmail from '../../utils/sendEmail'
 
 export default async (req, res) => {
-  // console.log(req.body)
+  console.log(req.body)
   const { name, email, number, subject, text } = req.body
 
-  const data = {
-    // Update your email here
-    to: 'info@ljt.app',
-    from: email,
-    subject: 'Hi there',
-    text: text,
-    html: `
-            <b>From:</b> ${name} <br /> 
-            <b>Number:</b> ${number} <br /> 
-            <b>Subject:</b> ${subject} <br /> 
-            <b>Message:</b> ${text} 
-        `,
-  }
+  // const data = {
+  //   // Update your email here
+  //   to: 'info@ljt.app',
+  //   from: email,
+  //   subject: 'Hi there',
+  //   text: text,
+  //   html: `
+  //           <b>From:</b> ${name} <br />
+  //           <b>Number:</b> ${number} <br />
+  //           <b>Subject:</b> ${subject} <br />
+  //           <b>Message:</b> ${text}
+  //       `,
+  // }
+
+  // console.log(data)
+  const data = { name, email, number, subject, text }
+
   try {
-    const response = await mailer.sendMail(data)
+    const response = await sendEmail(data)
     console.log(response)
     res.status(200).send('Email send successfully')
   } catch (error) {
