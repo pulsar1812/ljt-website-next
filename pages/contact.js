349a71de980003f8b8fac753f35d271b04bbc846
pulsar1812 <cluster000@gmail.com>
2022-11-02 12:24:51 +0800

    Remove redundant import

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from '../components/Layouts/Navbar'
 import NavbarTwo from '../components/Layouts/NavbarTwo'
 import PageBanner from '../components/Common/PageBanner'
 import ContactInfo from '../components/Contact/ContactInfo'
